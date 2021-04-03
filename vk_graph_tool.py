48f03c5270d6dfa6da3596be00509f939ee08f76
Ladrillin <oleg.kernickiy@mail.ru>
2021-04-04 01:04:35 +0300

    Допилил оптимизацию с заменой try в рисовальщике графа. Выделил это в отдельную функцию. Функцию поиска скрытых друзей тоже выделил в отдельную функцию и убрал её из графа. Теперь чисто текстовая информация по скрытым.

--- vk_graph_tool.py
+++ vk_graph_tool.py
@@ -1,31 +1,48 @@
 import networkx as nx
 import matplotlib.pyplot as mpl
-from vk_api.exceptions import ApiError
-import json
-
 from api import vk
 
 
-def parse_friends(root_id, root_friendlist):
+def if_closed(user_list):
+    info = vk.users.get(user_ids=user_list)
+    check_if_closed = dict()
+    for user in info:
+        user_id = user['id']
+        if 'deactivated' in user:
+            check_if_closed[user_id] = False
+            continue
+        user_closed_state = user["can_access_closed"]
+        check_if_closed[user_id] = user_closed_state
+    return check_if_closed  # Словарь вида 'id': True/False - закрыт/открыт профиль (либо удален)
+
+
+def graph_drawer(root_id, root_friendlist):
     graph_network = nx.Graph()
     graph_network.add_node(root_id)
     edge_counter = 0
-    check_if_closed = vk.users.get(user_ids=root_friendlist['items'])
-    index = 0
+    check_if_closed = if_closed(root_friendlist['items'])
 
     for friend_id in root_friendlist['items']:  # Связь рута со всеми его друзьями
+        print('Drawing the first layer of Graph')
         graph_network.add_edge(root_id, friend_id)
 
     for root_mutual_friend_id in root_friendlist['items']:  # Связи между друзьями рута
-        try:  # Замена try, возможно полетит по пизде в будущем
+        print('Drawing the second layer of Graph')
+        if check_if_closed[root_mutual_friend_id]:
             get_mutual = vk.friends.getMutual(source_uid=root_id,
                                               target_uid=root_mutual_friend_id)
             for mutual_friend in get_mutual:
                 graph_network.add_edge(root_mutual_friend_id, mutual_friend)
                 edge_counter += 1
-        except:
+        else:
             continue
+    nx.draw(graph_network, with_labels=True,
+            font_color='g', font_size=12, font_weight='bold')
+    print(edge_counter)
+    return mpl.show()
 
+
+def find_hidden_friends(root_id, root_friendlist):  # Очень долго
     print('Getting into hidden layer')
     for friend_layer1 in root_friendlist['items']:
         print('Признаки жизни')
@@ -39,15 +56,9 @@ def parse_friends(root_id, root_friendlist):
             except:
                 continue
             if root_id in layer2_list['items'] and friend_layer2 not in root_friendlist['items']:
-                graph_network.add_edge(root_id, friend_layer2)
                 print(friend_layer2)
-
-    nx.draw(graph_network, with_labels=True,
-            font_color='g', font_size=12, font_weight='bold')
-    print(edge_counter)
-    return mpl.show()
 
 
 source_id = 141498951
 source_friends = vk.friends.get(user_id=source_id,)
-parse_friends(source_id, source_friends)
+graph_drawer(source_id, source_friends)
